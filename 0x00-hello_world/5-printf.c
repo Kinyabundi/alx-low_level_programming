aa2e0efcc09287b47c9e9123a2a9fdc80d121293
kinyabundi <kinyachristine14@gmail.com>
2022-06-30 10:47:21 -0700

    printing using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *Return: Always 0 (run successifully)
  */
 int main(void)
-   {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-   }
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
