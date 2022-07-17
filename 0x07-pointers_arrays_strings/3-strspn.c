ba6af200fd3af7e5352ce46f83125387203910a6
Kinyabundi <kinyachristine14@gmail.com>
2022-07-17 12:09:57 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - Function
+ *
+ * Description: gets the length of a prefix substring
+ *
+ * @s: pointer parameter of type char
+ * @accept: pointer parameter of type char
+ *
+ * Return: returns bytes in unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	char *reset = s;
+
+	while (*accept)
+	{
+		while (*s)
+		{
+			if (*s == *accept)
+			{
+				bytes++;
+				break;
+			}
+			s++;
+		}
+		accept++;
+		s = reset;
+
+	}
+	return (bytes);
+}
