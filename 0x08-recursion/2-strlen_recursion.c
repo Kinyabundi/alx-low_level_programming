3fa8c672019781e2df209c4522fa599d6d7584b6
Kinyabundi <kinyachristine14@gmail.com>
2022-07-21 16:58:56 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be inputed
+ * Return: Always 0 (epitychia)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	return (_strlen_recursion(s + 1) + 1);
+}
